45f2eae61aa26526c189eb89b8bcdb7ac75084f1
Lysak Tatiana <lysak.tatiana2501@ya.ru>
2020-04-10 10:07:20 +0300

    Fix bugs and added functional for creating task

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-  const caseTasks = $('.js-tasks');
+  const caseTasksEl = $('.js-tasks');
   const tasksListEl = $('.js-tasksList');
   const taskFormEl = $('.js-taskForm');
 
@@ -19,4 +19,33 @@ $(function () {
               </article>
             </li>`;
   }
+
+  function checkIsTaskLestEmpty() {
+    // Удаление отступов и текса, если в списке есть задачи
+    if(tasksListEl.children('.js-taskItem').length > 0) {
+      $('.empty-list-text').remove();
+      caseTasksEl.removeClass('empty-list');
+      taskFormEl.removeClass('task-form-empty-list');
+    }
+
+    // Добавление отступов и текса, если в списке нет задач
+    if(tasksListEl.children('.js-taskItem').length === 0) {
+      caseTasksEl.prepend('<p class="empty-list-text">Список пуст...</p>');
+      caseTasksEl.addClass('empty-list');
+      taskFormEl.addClass('task-form-empty-list');
+    }
+  }
+
+  taskFormEl.submit(function( event ) {
+    event.preventDefault();
+
+    let target = event.target;
+    let taskName = $('.task-name').val();
+    let taskDesk = $('.task-desk').val();
+    let newTaskEl = buildTask(taskName, taskDesk);
+    tasksListEl.prepend(newTaskEl);
+
+    target.reset(); // Сброс формы
+    checkIsTaskLestEmpty(); // Проверка списка задач
+  });
 });
